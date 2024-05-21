99d7756bed1dfa79c5c607ff829eab862d175e94
sevensinjai <45555353+sevensinjai@users.noreply.github.com>
2024-05-21 12:45:22 +0800

    feat: add python script for generating app data

---
+++ data_generation/main.py
@@ -0,0 +1,36 @@
+import pycantonese
+import os
+
+if __name__ == "__main__":
+    result = []
+    # read the files
+    for file in os.listdir("./.data/input"):
+        with open("./.data/input/{}".format(file), "r") as f:
+            for line in f:
+                for word in line:
+                    if word == "\n" or word == " ":
+                        result[-1]['suffix'] = word
+                        continue
+                    if word == "":
+                        continue
+                    pingying = pycantonese.characters_to_jyutping(word)
+                    pingying = pingying[0]
+                    parsedPingying = pycantonese.parse_jyutping(pingying[1])
+
+                    onset = parsedPingying[0].onset
+                    final = parsedPingying[0].nucleus + parsedPingying[0].coda
+                    tone = parsedPingying[0].tone
+                    result.append(
+                        {
+                            "initial": onset,
+                            "final": final,
+                            "word": word,
+                            "tone": tone
+                        }
+                    )
+
+        # dump to json with chinese character
+        import json
+        json_file_name = file.split(".")[0] + ".json"
+        with open("./.data/output/{}".format(json_file_name), "w") as f:
+            json.dump(result, f, ensure_ascii=False)
