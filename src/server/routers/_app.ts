9a997c950f677c4a4115f17f1bf42f59d100a5e3
sevensinjai <45555353+sevensinjai@users.noreply.github.com>
2024-05-21 12:43:37 +0800

    chore: remove example related code

--- src/server/routers/_app.ts
+++ src/server/routers/_app.ts
@@ -2,12 +2,9 @@
  * This file contains the root router of your tRPC-backend
  */
 import { createCallerFactory, publicProcedure, router } from '../trpc';
-import { postRouter } from './post';
 
 export const appRouter = router({
   healthcheck: publicProcedure.query(() => 'yay!'),
-
-  post: postRouter,
 });
 
 export const createCaller = createCallerFactory(appRouter);
