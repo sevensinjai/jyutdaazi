cd30cd26511b6ad02bf5064f2afe4b7df42afe3f
sevensinjai <45555353+sevensinjai@users.noreply.github.com>
2024-05-23 22:22:57 +0800

    fix: use fs to read files instead of import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import ParagraphCanvas from "@/features/ParagraphCanvas"
 import { StoreProvider } from "@/features/ParagraphCanvas/store"
 import EndingDialog from "@/features/ParagraphCanvas/EndingDialog"
 import type { InferGetServerSidePropsType, GetServerSideProps } from 'next'
+import { promises as fs } from 'fs';
 
 
 export type ParagraphWord = {
@@ -14,9 +15,10 @@ export type ParagraphWord = {
 }
 export const getServerSideProps = (async () => {
   // random int from 1 - 1
-  // const randomInt = Math.floor(Math.random() * 5) + 1
+  const randomInt = Math.floor(Math.random() * 5) + 1
   // import the paragraph from the json file
-  const { default: data } = await import(`@/server/paragraphs/1.json`, { assert: { type: 'json' } })
+  const file = await fs.readFile(process.cwd() + `/src/server/paragraphs/${randomInt}.json`, 'utf8');
+  const data = JSON.parse(file)
   const paragraph: ParagraphWord[] = data
   return { props: { paragraph } }
 }) satisfies GetServerSideProps<{ paragraph: ParagraphWord[] }>
